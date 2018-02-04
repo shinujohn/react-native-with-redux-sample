5722f054d51fc15091c3ce465652d538563b4e61
girishg4t <talekar.g@gmail.com>
2018-02-04 19:37:14 +0530

    corrected test case

--- automationTest/sampleTest.js
+++ automationTest/sampleTest.js
@@ -1,11 +1,8 @@
-
 var wd = require('wd');
 var WdAndroid = require('wd-android');
 var assert = require('assert');
 var asserters = wd.asserters;
 
-var async = require('asyncawait/async');
-var await = require('asyncawait/await');
 
 desiredCaps = {
     'browserstack.user': 'markwilson5',
@@ -13,37 +10,26 @@ desiredCaps = {
     'build': 'Node Android',
     'name': 'single_test',
     'device': 'Google Pixel',
-    'app':'bs://13011a4300946cce2440cd2695d51874bd9ad303',
+    'app': 'bs://13011a4300946cce2440cd2695d51874bd9ad303',
     'browserstack.debug': true
 };
-
-//  driver = wd.promiseRemote("http://hub-cloud.browserstack.com/wd/hub");
-
-// driver
-//     .init(desiredCaps)
-//     .then(function () {
-//         return driver.waitForElementByAccessibilityId('signUp', asserters.isDisplayed && asserters.isEnabled, 30000);
-//     })
-//     .then(function (searchElement) {
-//         return searchElement.click();
-//     }).fin(function () { return driver.quit(); })
-//     .done();
-
 
 describe("Using Appium and WdAndroid to test Android App.", function () {
     this.timeout(300000);
     var driver,
         allPassed = true;
 
-    before(async function () {
+    before(function () {
 
         var wdAndroid = new WdAndroid(wd);
+        return new Promise((res, rej) => {
+            driver = wdAndroid.promiseChainRemote("http://hub-cloud.browserstack.com/wd/hub");
+            driver.init(desiredCaps)
+                .setImplicitWaitTimeout(10000).then(() => {
+                    res();
+                });
+        });
 
-        driver = await wdAndroid.promiseChainRemote("http://hub-cloud.browserstack.com/wd/hub");
-
-        return driver
-            .init(desiredCaps)
-            .setImplicitWaitTimeout(10000);
     });
 
 
@@ -57,42 +43,44 @@ describe("Using Appium and WdAndroid to test Android App.", function () {
     });
 
 
-    it("should find an element", async function () {
-
-        let elName = await driver.elementsByAccessibilityId('name');
-        let elEmail = await driver.elementsByAccessibilityId('email');
-        let el = await driver.elementsByAccessibilityId('signUp');
-        //let elBackButton = await driver.elementsById('navigationBarBackground');
-        // android:id/navigationBarBackground
-        await elName[0].setText("Girish Talekar").click().back();
-        await elEmail[0].setText("talekar.g@gmail.com").click().back();
-        return el[0].click().sleep(4000);
+    it("should find an element and save the details", function () {
+        return new Promise((res, rej) => {
+            driver.waitForElementByAccessibilityId('name', asserters.isDisplayed && asserters.isEnabled, 30000)
+                .then((elName) => {
+                    return elName.setText("Girish Talekar").click().back();
+                })
+                .then(function () {
+                    return driver.waitForElementByAccessibilityId('email', asserters.isDisplayed && asserters.isEnabled, 30000);
+                })
+                .then(function (elEmail) {
+                    return elEmail.setText("talekar.g@gmail.com").click().back();
+                })
+                .then(function () {
+                    return driver.waitForElementByAccessibilityId('password', asserters.isDisplayed && asserters.isEnabled, 30000);
+                })
+                .then(function (elPassword) {
+                    return elPassword.setText("password").click().back();
+                })
+                .then(function () {
+                    return driver.waitForElementByAccessibilityId('phone', asserters.isDisplayed && asserters.isEnabled, 30000);
+                })
+                .then(function (elPhone) {
+                    return elPhone.setText("8380083849").click().back();
+                })
+                .then(function () {
+                    return driver.waitForElementByAccessibilityId('address', asserters.isDisplayed && asserters.isEnabled, 30000);
+                })
+                .then(function (elAddress) {
+                    return elAddress.setText("Capita India").click().back();
+                })
+                .then(function () {
+                    return driver.waitForElementByAccessibilityId('signUp', asserters.isDisplayed && asserters.isEnabled, 30000);
+                })
+                .then(function (searchElement) {
+                    return searchElement.click().sleep(4000);
+                }).fin(function () { res();})
+                .done();
+        });
     });
 
 });
-
-/*
-driver
-    .init(desiredCaps)
-    .then(function () {
-        return driver.waitForElementByAccessibilityId('Search Wikipedia', asserters.isDisplayed && asserters.isEnabled, 30000);
-    })
-    .then(function (searchElement) {
-        return searchElement.click();
-    })
-    .then(function () {
-        return driver.waitForElementById('org.wikipedia.alpha:id/search_src_text', asserters.isDisplayed && asserters.isEnabled, 30000);
-    })
-    .then(function (searchInput) {
-        return searchInput.sendKeys("BrowserStack");
-    })
-    .then(function () {
-        return driver.elementsByClassName('android.widget.TextView');
-    })
-    .then(function (search_results) {
-        assert(search_results.length > 0);
-    })
-    .fin(function () { return driver.quit(); })
-    .done();
-
-    */
